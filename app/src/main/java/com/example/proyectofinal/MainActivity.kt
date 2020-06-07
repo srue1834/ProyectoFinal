ef877efa3406255426249269c7fb6aa346df824a
srue1834 <rue19099@uvg.edu.gt>
2020-06-07 17:37:25 -0600

    cambios en fase1 y base de datos room

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.proyectofinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/proyectofinal/fase1/Fase1.kt
+++ app/src/main/java/com/example/proyectofinal/fase1/Fase1.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-
 import com.example.proyectofinal.R
 import com.example.proyectofinal.databinding.FragmentFase1Binding
 
@@ -13,6 +13,9 @@ import com.example.proyectofinal.databinding.FragmentFase1Binding
  * A simple [Fragment] subclass.
  */
 class Fase1 : Fragment() {
+
+    private lateinit var fase1ViewModel: Fase1ViewModel
+    private lateinit var binding: FragmentFase1Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +31,16 @@ class Fase1 : Fragment() {
             view.findNavController().navigate(R.id.action_fase1_to_fase2)
         }
 
+        binding.registrarse.setOnClickListener {
+            val usuario = binding.usuario2.text.toString()
+            val contra = binding.contra2.text.toString()
+
+            fase1ViewModel.crearCuenta(usuario, contra)
+        }
+
+        binding.lifecycleOwner = this
         setHasOptionsMenu(true)
+
         return binding.root
     }
 
@@ -37,5 +49,17 @@ class Fase1 : Fragment() {
         inflater?.inflate(R.menu.navdrawer_menu, menu)
 
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        fase1ViewModel = ViewModelProvider(this).get(Fase1ViewModel::class.java)
+        binding.fase1ViewModel = fase1ViewModel
+
+
+    }
+
+
 
 }
